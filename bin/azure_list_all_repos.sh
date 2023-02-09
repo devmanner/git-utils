1c74d7f74b75de0e064cb99425faad193d9f1bd7
Tomas Mannerstedt <tmannerstedt@hotmail.com>
2023-02-09 13:17:33 +0100

    Added first version of listing azure repos

---
+++ bin/azure_list_all_repos.sh
@@ -0,0 +1,32 @@
+#!/opt/homebrew/bin/bash
+
+PAT_FILE=~/.azure_pat_access_token
+
+if [ ! -f $PAT_FILE ]; then
+    echo "You need a PAT (Personal Access Token) stored in $PAT_FILE"
+    exit -1
+fi
+
+PAT=$(cat $PAT_FILE)
+
+if [ $# != 2 ]; then
+    echo "Usage: $0 username organization"
+    exit -1
+fi
+
+USER=$1
+ORG=$2
+
+HEADER='-H "Content-Type: application/json"'
+
+CURL="curl -s -u $USER:$PAT $HEADER"
+
+LIST_PROJS="https://dev.azure.com/$ORG/_apis/projects?api-version=2.0"
+
+for proj in $($CURL $LIST_PROJS | jq -r '.value[].name'); do
+    LIST_REPOS="https://dev.azure.com/$ORG/$proj/_apis/git/repositories?api-version=7.0"
+    for repo in $($CURL $LIST_REPOS | jq -r '.value[].sshUrl'); do
+        echo $repo
+    done
+done
+
